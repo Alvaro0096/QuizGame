354112df97f8f690a1295eb2ee3b5a3056506ce8
AlvaroJorge <alv.jor96@gmail.com>
2021-12-31 13:43:06 -0300

    subiendo js finalizado

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let puntaje = document.getElementById("puntaje");
-let txt_pregunta = document.querySelector(".pregunta_p");
+let pregunta_txt = document.querySelector(".pregunta_p");
 
 let txt_preg_a = document.querySelector(".res_a");
 let txt_preg_b = document.querySelector(".res_b");
 let txt_preg_c = document.querySelector(".res_c");
+let div_txt = document.querySelectorAll(".res");
 
 let btn_a = document.querySelector(".caja_a");
 let btn_b = document.querySelector(".caja_b");
@@ -13,63 +14,177 @@ let check_a = document.querySelector(".check_a");
 let check_b = document.querySelector(".check_b");
 let check_c = document.querySelector(".check_c");
 
+let btn_res = document.querySelectorAll('button');
+
+let img_ok = document.querySelector('.img_good');
+let img_bad = document.querySelector('.img_bad');
+
 let continuar = document.querySelector('.continuar');
 
-let contador = 0;
+let contador_pregunta = 1;
+let score = 0;
 
-let pregunta_primera = 
+let pregunta1 = 
 {
 	pregunta:"La segunda guerra mundial fue el conflicto bélico más devastador en la historia de la humanidad. ¿Cúal fue la batalla con mayores bajas del conflicto?",
 	res_obj_a:"Batalla de Las Ardenas",
 	res_obj_b:"Batalla de Stalingrado",
 	res_obj_c:"Batalla de Berlin",
-	correcta: "Batalla de Stalingrado"
+	correcta: "Batalla de Stalingrado",
+	check: false
 }
 	
-let pregunta_segunda =	
+let pregunta2 =	
 {
 	pregunta:"Al finalizar la segunda gurra mundial dos paises surgieron como las nuevas Super Potencias ¿Cuales eran estos paises?",
 	res_obj_a:"China y URSS",
 	res_obj_b:"Reino Unido y USA",
 	res_obj_c:"USA y URSS",
-	correcta:"USA y URSS"
+	correcta:"USA y URSS",
+	check: false
 }
 
-let pregunta_tercera =	
+let pregunta3 =	
 {
 	pregunta:"La caída de Constantinopla a mano de los turcos otomanos significó el fin de la edad media. ¿En que año ocurrió?",
 	res_obj_a:"1453",
 	res_obj_b:"1383",
 	res_obj_c:"1523",
-	correcta:"1453"
+	correcta:"1453",
+	check: false
 }
  
-function mostrarPregunta_a(){
-	txt_pregunta.innerHTML = pregunta_primera.pregunta;
-	txt_preg_a.innerHTML = pregunta_primera.res_obj_a;
-	txt_preg_b.innerHTML = pregunta_primera.res_obj_b;
-	txt_preg_c.innerHTML = pregunta_primera.res_obj_c;
+function mostrarPregunta(pregunta){
+	pregunta_txt.innerHTML = pregunta.pregunta;
+	txt_preg_a.innerHTML = pregunta.res_obj_a;
+	txt_preg_b.innerHTML = pregunta.res_obj_b;
+	txt_preg_c.innerHTML = pregunta.res_obj_c;
+
 };
 
-function mostrarPregunta_b(){
-	txt_pregunta.innerHTML = pregunta_segunda.pregunta;
-	txt_preg_a.innerHTML = pregunta_segunda.res_obj_a;
-	txt_preg_b.innerHTML = pregunta_segunda.res_obj_b;
-	txt_preg_c.innerHTML = pregunta_segunda.res_obj_c;
-};
+if (contador_pregunta===1) {
+	mostrarPregunta(pregunta1);
+} 
 
-function mostrarPregunta_c(){
-	txt_pregunta.innerHTML = pregunta_tercera.pregunta;
-	txt_preg_a.innerHTML = pregunta_tercera.res_obj_a;
-	txt_preg_b.innerHTML = pregunta_tercera.res_obj_b;
-	txt_preg_c.innerHTML = pregunta_tercera.res_obj_c;
-};
+function comprobar(respuesta, pregunta){
+	if (respuesta===pregunta.correcta) {
+		txt_preg_a.disabled=true;
+		txt_preg_b.disabled=true;
+		txt_preg_c.disabled=true;
+		continuar.style.display = 'flex';
+		score = score + 5;
+		puntaje.textContent = score;
+		pregunta.check = true
+		console.log(pregunta.check);
+		color_back(pregunta);
+	}
+	else{
+		txt_preg_a.disabled=true;
+		txt_preg_b.disabled=true;
+		txt_preg_c.disabled=true;
+		continuar.style.display = 'flex';
+		score = score - 5;
+		puntaje.textContent = score;
+		console.log(pregunta.check);
+		color_back(pregunta);
+	}	
+	
+}
+
+function color_back(pregunta){
+		if (contador_pregunta === 1) {
+			btn_a.classList.add('red');
+			btn_b.classList.add('green');
+			btn_c.classList.add('red');
+			check_a.textContent = 'Respuesta Incorrecta!';
+			check_b.textContent = 'Respuesta Correcta!';
+			check_c.textContent = 'Respuesta Incorrecta!';
+		}
+		else if(contador_pregunta === 2){
+			btn_a.classList.add('red');
+			btn_b.classList.add('red');
+			btn_c.classList.add('green');
+			check_a.textContent = 'Respuesta Incorrecta!';
+			check_b.textContent = 'Respuesta Incorrecta!';
+			check_c.textContent = 'Respuesta Correcta!';
+		}
+		else if(contador_pregunta === 3){
+			btn_a.classList.add('green');
+			btn_b.classList.add('red');
+			btn_c.classList.add('red');
+			check_a.textContent = 'Respuesta Correcta!';
+			check_b.textContent = 'Respuesta Inorrecta!';
+			check_c.textContent = 'Respuesta Incorrecta!';
+		}
+
+	}
+
+function color_back_remove(pregunta){
+		if (contador_pregunta === 1) {
+			btn_a.classList.remove('red');
+			btn_b.classList.remove('green');
+			btn_c.classList.remove('red');
+			check_a.textContent = '';
+			check_b.textContent = '';
+			check_c.textContent = '';
+			continuar.style.display = 'none';
+		}
+		else if(contador_pregunta === 2){
+			btn_a.classList.remove('red');
+			btn_b.classList.remove('green');
+			btn_c.classList.remove('red');
+			check_a.textContent = '';
+			check_b.textContent = '';
+			check_c.textContent = '';
+			continuar.style.display = 'none';
+		}
+		else if(contador_pregunta === 3){
+			btn_a.classList.remove('red');
+			btn_b.classList.remove('red');
+			btn_c.classList.remove('green');
+			check_a.textContent = '';
+			check_b.textContent = '';
+			check_c.textContent = '';
+			continuar.style.display = 'none';
+		}
+
+	}
+	
 
 
-function preguntas(){
-	if (mostrarPregunta_a == true) {
-		mostrarPregunta_a();
-	} else{
-		mostrarPregunta_b()
+btn_res.forEach(element => {
+    element.addEventListener('click', (e)=>{
+        if (contador_pregunta===1){
+        	comprobar(e.target.innerHTML, pregunta1);
+        }
+        else if(contador_pregunta===2){
+        	comprobar(e.target.innerHTML, pregunta2);
+        }
+        else if(contador_pregunta===3){
+        	comprobar(e.target.innerHTML, pregunta3);
+        }
+    });
+})
+
+function btn_cambio_pag(){
+	contador_pregunta = contador_pregunta + 1;
+	txt_preg_a.disabled=false;
+	txt_preg_b.disabled=false;
+	txt_preg_c.disabled=false;
+	color_back_remove()
+	if (contador_pregunta===2) {
+		mostrarPregunta(pregunta2);
+		pregunta2.check = false;
+	}
+	else if(contador_pregunta===3){
+		mostrarPregunta(pregunta3);
+		pregunta3.check = false;
+	}
+	else {
+		alert(`Juego Finalizado.`);
 	}
 }
+
+continuar.addEventListener('click', () => {
+	btn_cambio_pag();
+})
